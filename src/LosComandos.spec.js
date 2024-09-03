5a7a28624887c0f33a0920725d3fd1eeabca38d5
Vivian Revollo <revollovivian@gmail.com>
2024-09-03 14:13:14 -0400

    Añadiendo clase superficie

--- src/LosComandos.spec.js
+++ src/LosComandos.spec.js
@@ -1,8 +1,17 @@
 import mostrarLosComandos from "./LosComandos.js";
+import Superficie from "./Superficie.js";
+
 
 describe("Comandos del auto para que se defina el grid y se inicialice el auto", () => {
   it("Se deberia mostrar el comando ingresado por el usuario", () => {
     const result = mostrarLosComandos("5,5/1,2N/IAIAIAIAA")
     expect(result).toEqual("5,5/1,2N/IAIAIAIAA");
+  });
+});
+
+describe("Superficie", () => {
+  it("Deberia mostrar el area de la superfice", () => {
+    const result = new Superficie(3, 3)
+    expect(result.mostrarSuperficie()).toEqual([3, 3]);
   });
 });
---
+++ src/Superficie.js
@@ -0,0 +1,12 @@
+class Superficie {
+    constructor(filas, columnas) {
+      this.filas = filas;
+      this.columnas = columnas;
+    }
+
+    mostrarSuperficie() {
+        return [this.filas, this.columnas];
+    }
+  }
+
+  export default Superficie;
