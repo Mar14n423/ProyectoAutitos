a7693783f732a2afe40667f53851d581ae4e1df2
Mar14n423 <wara.mercado@ucb.edu.bo>
2024-09-03 21:48:00 -0400

    Revert "verificación si las columas son mayores a 0 para ser valido"
    This reverts commit 3de72b1838a4a20c05811739414d6fdb954c8d01.

--- src/LosComandos.spec.js
+++ src/LosComandos.spec.js
@@ -14,16 +14,12 @@ describe("Superficie", () => {
     const superficie = new Superficie(3, 3)
     expect(superficie.mostrarSuperficie()).toEqual([3, 3]);
   });
-  it("Debería verificar si las filas y las columnas son mayores a 0", () => {
+  it("Debería verificar si las filas son mayores a 0", () => {
     const superficie = new Superficie(3, 3);
     expect(superficie.validarLaSuperficie()).toEqual(true);
   });
   it("Debería verificar si la superficie no es válida (que las filas sean menores a 0)", () => {
     const superficie = new Superficie(-5, 3);
-    expect(superficie.validarLaSuperficie()).toEqual(false);
-  });
-  it("Debería verificar si la superficie no es válida (que las columnas sean menores a 0)", () => {
-    const superficie = new Superficie(3, 0);
     expect(superficie.validarLaSuperficie()).toEqual(false);
   });
 });
